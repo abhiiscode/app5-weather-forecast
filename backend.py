7d5e801cf4b7f7404c7f7978fac1feb0cfeb4622
abhiiscode <abhismega@gmail.com>
2026-03-12 11:42:48 +0530

    Initial commit - App7 Weather Forecast

---
+++ backend.py
@@ -0,0 +1,20 @@
+import requests
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(Path(__file__).parent / "api.env")
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    responce = requests.get(url)
+    data = responce.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3, kind="temperatures"))
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 # .express is the module present in plotly
 
-
-st.title("Weather forcast for the Next Days")
+# Add title, text input, slider, selextbox, subheader
+st.title("Weather forcast for the Next 5 Days")
 place = st. text_input("Place: ")
 days = st.slider("forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
@@ -11,15 +12,29 @@ option = st.selectbox("Select date to view",
                      ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2024-07-01", "2024-07-02", "2024-07-03",]
-    temperatures = [10, 11, 15]
-    # list comprehestion
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+if place:
+    try:
+    # Get the temperature/Sky date
+        filtered_data = get_data(place, days)
 
-date, temp = get_data(days)
+        if option == "Temperature":
+            # Create a temperature plot
+            temperatures = [dict["main"]['temp'] / 10    for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures,
+                             labels={"x": "Date", "y": "Temperature (c)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=date, y=temp,
-                 labels={"x": "Date", "y": "Temperature (c)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            # Create a temperature plot
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds": "images/clouds.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            # Transleting the data
+            print(sky_condition)
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path, width=115)
+    except KeyError:
+        st.info("You Entered Wrong City Name,"
+                " Name start's with capital later")
+
+
